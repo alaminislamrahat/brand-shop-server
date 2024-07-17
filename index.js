717f368a6e3fa0c94f7d1834324129619938f475
unknown <alamin>
2024-07-17 18:00:45 +0600

    finish my work

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -18,7 +19,8 @@ app.use(express.json());
 
 
 // const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://alaminislamrahat:0dIFQbljXpJRCIDu@cluster0.3jkraio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3jkraio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -35,6 +37,31 @@ async function run() {
         await client.connect();
 
         const productCollection = client.db("productDB").collection("product");
+        const addedCollection = client.db("productDB").collection("cart");
+
+        // cart section 
+
+        app.post('/cart', async (req, res) =>{
+            const data = req.body;
+            const result = await addedCollection.insertOne(data);
+            res.send(result);
+        })
+
+
+        app.get('/cart', async (req, res) =>{
+            const result = await addedCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.delete('/cart/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: id };
+            console.log(query)
+            const result = await addedCollection.deleteOne(query);
+            console.log(result)
+            res.send(result);
+        })
 
         // product section 
 
@@ -47,6 +74,41 @@ async function run() {
         app.get('/products', async (req, res) => {
             const result = await productCollection.find().toArray();
             res.send(result);
+        })
+
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = {_id : new ObjectId(id)};
+            const result = await productCollection.deleteOne(query);
+            res.send(result)
+        })
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};
+            const result = await productCollection.findOne(query);
+            res.send(result)
+        })
+
+        app.put('/products/:id', async (req, res) => {
+            const data = req.body;
+            const id = req.params.id;
+            const filter = {_id : new ObjectId(id)};
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set:{
+                    brandName : data.brandName, 
+                    image : data.image, 
+                    name : data.name, 
+                    price : data.price, 
+                    rating : data.rating, 
+                    type : data.type
+                }
+            }
+            const result = await productCollection.updateOne(filter,updatedDoc,options);
+            res.send(result);
+
         })
        
         await client.db("admin").command({ ping: 1 });
